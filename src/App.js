e17bf3d20ab9cf05695d3c7fe32c7020546da1c3
Seunghwan <seunghwansohn@gmail.com>
2020-01-07 17:29:54 +0700

    CSS 모듈 테스트중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule'
 
 function App() {
   return (
     <div className="App">
       <SassComponent/>
+      <CSSModule/>
     </div>
   );
 }
